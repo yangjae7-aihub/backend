926f8930d600ea31c8a0287616e6634008918b0f
Jin <rifampicin@naver.com>
2021-08-05 12:06:07 +0000

    build

--- moodmaker/settings.py
+++ moodmaker/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'frontend/build'
+            BASE_DIR / 'build'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -129,7 +129,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'frontend/build/static',
+    BASE_DIR / 'build/static',
 ]
 
 # Default primary key field type
