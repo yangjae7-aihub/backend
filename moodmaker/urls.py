4b9469fe83080cdf9a37484eae1c10d007035102
Jin <rifampicin@naver.com>
2021-08-06 00:32:26 +0000

    url

--- moodmaker/urls.py
+++ moodmaker/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('diary/', include('diary_app.urls')),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('main', TemplateView.as_view(template_name='index.html'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
